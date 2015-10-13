cedf5e768c8ca2fa5ab597543c05424ba20a7af0
quartz55 <john.razor97@gmail.com>
2015-10-13 17:44:06 +0100

    Better makefile
    Makefile fix
    
    Better structure

--- Trabalho1/noncanonical.c
+++ Trabalho1/noncanonical.c
@@ -23,49 +23,56 @@
 volatile int STOP=FALSE;
 int main(int argc, char** argv)
 {
-	int fd,c, res;
-	struct termios oldtio,newtio;
-	char buf[255];
-	unsigned char UA[5];
-	UA[0]=FLAG;
-	UA[1]=A;
-	UA[2]=C_UA;
-	UA[3]=A^C_UA;
-	UA[4]=FLAG;
+  int fd,c, res;
+  struct termios oldtio,newtio;
+  char buf[255];
+  unsigned char UA[5];
+  UA[0]=FLAG;
+  UA[1]=A;
+  UA[2]=C_UA;
+  UA[3]=A^C_UA;
+  UA[4]=FLAG;
 
+<<<<<<< HEAD
 	if(argc < 2) {
 	printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
 	exit(1);
 	}
+=======
+  if (argc < 2) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+      exit(1);
+  }
+>>>>>>> Better makefile
 
 /*
-Open serial port device for reading and writing and not as controlling tty
-because we don't want to get killed if linenoise sends CTRL-C.
+  Open serial port device for reading and writing and not as controlling tty
+  because we don't want to get killed if linenoise sends CTRL-C.
 */
-	fd = open(argv[1], O_RDWR | O_NOCTTY );
-	if (fd <0) { perror(argv[1]); exit(-1); }
-	tcgetattr(fd,&oldtio); /* save current port settings */
-	bzero(&newtio, sizeof(newtio));	
-	newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-	newtio.c_iflag = IGNPAR;
-	newtio.c_oflag = 0;
+  fd = open(argv[1], O_RDWR | O_NOCTTY );
+  if (fd <0) { perror(argv[1]); exit(-1); }
+  tcgetattr(fd,&oldtio); /* save current port settings */
+  bzero(&newtio, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
 
 /* set input mode (non-canonical, no echo,...) */
-	newtio.c_lflag = 0;
-	newtio.c_cc[VTIME] = 0; /* inter-character timer unused */
-	newtio.c_cc[VMIN] = 5; /* blocking read until 5 chars received */
-	tcflush(fd, TCIFLUSH);
-	tcsetattr(fd,TCSANOW,&newtio);
+  newtio.c_lflag = 0;
+  newtio.c_cc[VTIME] = 0; /* inter-character timer unused */
+  newtio.c_cc[VMIN] = 5; /* blocking read until 5 chars received */
+  tcflush(fd, TCIFLUSH);
+  tcsetattr(fd,TCSANOW,&newtio);
 /* printf("New termios structure set\n"); */
 
-	res=0;
-	while (res== 0) { /* loop for input */
-		res=read(fd,buf,255);
-	}
-	printf("Message Read\n");
-	res = write(fd, UA, res);
-	printf("Message resent\n");
-	tcsetattr(fd,TCSANOW,&oldtio);
-	close(fd);
-	return 0;
+  res=0;
+  while (res== 0) { /* loop for input */
+      res=read(fd,buf,255);
+  }
+  printf("Message Read\n");
+  res = write(fd, UA, res);
+  printf("Message resent\n");
+  tcsetattr(fd,TCSANOW,&oldtio);
+  close(fd);
+  return 0;
 }
